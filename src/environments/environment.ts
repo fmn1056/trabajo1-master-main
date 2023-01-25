fd325e844275bd08b3e9174c884e1bc3eaeac896
fmn1056 <101664642+fmn1056@users.noreply.github.com>
2023-01-25 00:14:40 -0300

    Initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,29 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  API_URL:'https://api.openweathermap.org/data/2.5',
+  API_KEY:'526070db1d0b3652c20e9dfccbce1303',
+
+
+ firebaseConfig : {
+  apiKey: "AIzaSyCVrfoT8M9Qujyv8L27Shj4niml8wxq4l4",
+  authDomain: "proyeecto1-c5e24.firebaseapp.com",
+  projectId: "proyeecto1-c5e24",
+  storageBucket: "proyeecto1-c5e24.appspot.com",
+  messagingSenderId: "930950533948",
+  appId: "1:930950533948:web:40a3de9dc19bda59d8c439",
+  measurementId: "G-7X26F122MW"
+},
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
